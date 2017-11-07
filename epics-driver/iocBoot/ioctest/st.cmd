a07d3a1fe52bb21482358ea3dbaa3697a590cf50
root <root@mtcacpu03>
2017-11-07 15:33:56 -0500

    IOC updates, Version B110.  Includes DDR readback support.

--- epics-driver/iocBoot/ioctest/st.cmd
+++ epics-driver/iocBoot/ioctest/st.cmd
@@ -1,32 +1,39 @@
 #!../../bin/linux-x86_64-debug/pico
 
-#< envPaths
+< envPaths
 
-dbLoadDatabase("../../dbd/pico.dbd",0,0)
+cd $(TOP)
+
+epicsEnvSet("EPICS_CA_MAX_ARRAY_BYTES","8388648")
+
+dbLoadDatabase("dbd/pico.dbd",0,0)
 pico_registerRecordDeviceDriver(pdbbase) 
 
-createPICO8("PICO", "/dev/amc_pico_0000:01:00.0")
-#debugPICO("PICO", 5)
+#system("insmod ../linux-driver/amc_pico.ko")
 
-#dbLoadRecords("../../db/pico8.db","SYS=TST,D=pico,NAME=PICO")
-dbLoadRecords("../../db/pico8_frib.db","SYS=TST,D=pico,NAME=PICO")
+createPICO8("PICOD", "/dev/amc_pico_0000:06:00.0")
+#createPICO8("PICO3", "/dev/amc_pico_0000:07:00.0")
+dbLoadRecords("db/pico8_frib.db","SYS=TST,D=picod,NAME=PICOD,NELM=1048576")
+#dbLoadRecords("db/pico8_frib.db","SYS=TST,D=pico3,NAME=PICO3,NELM=1048576")
+#dbLoadRecords("db/pico8_ram.template", "P=TST:picod_mem:,NAME=PICOD,FTVL=LONG,NELM=600000")
 
-dbLoadRecords("../../db/pico8_ram.template", "P=TST:mem:,NAME=PICO,FTVL=LONG,NELM=7")
 
 # Auto save/restore
 save_restoreDebug(2)
 
-#dbLoadRecords("db/save_restoreStatus.db", "P=picotest:")
-#save_restoreSet_status_prefix("picotest:")
-
-set_savefile_path("${PWD}/as","/save")
-set_requestfile_path("${PWD}/as","/req")
+set_savefile_path("as","/save")
+set_requestfile_path("as","/req")
 
 set_pass0_restoreFile("pico_settings.sav")
 set_pass1_restoreFile("pico_waveforms.sav")
 
 system("install -d as/req")
 system("install -d as/save")
+
+
+## Start the PICO python helper script
+system "python iocBoot/scripts/blm_processing_thread.py TST:picod &"
+
 
 iocInit()
 
@@ -35,3 +42,11 @@ makeAutosaveFileFromDbInfo("as/req/pico_waveforms.req", "autosaveFields_pass1")
 
 create_monitor_set("pico_settings.req", 10 , "")
 create_monitor_set("pico_waveforms.req", 30 , "")
+
+# disable slew time difference between CPU and EVR
+var picoSlewLimit 9e99
+
+dbl > records.dbl
+
+dbpf "TST:picod_FPS:SLT_CSET", "7"    # Initialize PICO to AMC SLOT = 7
+# dbpf "TST:pico3_FPS:SLT_CSET", "3"    # Initialize PICO to AMC SLOT = 3
