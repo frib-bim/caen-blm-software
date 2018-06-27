7e9ddd330995b73e2dc133d9921c0bffcefa3e8c
Bruno Martins <martins@frib.msu.edu>
2018-06-27 11:28:34 -0400

    Add missing colons to aliases

--- epics-driver/iocBoot/ioc-ls1-mtca04-cpu-n0004/st.cmd
+++ epics-driver/iocBoot/ioc-ls1-mtca04-cpu-n0004/st.cmd
@@ -27,21 +27,21 @@ dbLoadRecords("../../db/pico8_frib.db","SYS=DIAG_MTCA04,D=PICO4,NAME=PICO4,NELM=
 # (SYS):(D)_CHX:Y_Z -> (A)Y_Z
 
 # Slot 3:  Neutron Detectors (BLM)
-dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO3_CH0:,A=FE_RFQ:ND_D1025")
-dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO3_CH1:,A=LS1_CA01:ND_D1128")
-dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO3_CH2:,A=LS1_CA03:ND_D1195")
-dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO3_CH4:,A=LS1_CB01:ND_D1250")
-dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO3_CH5:,A=LS1_CB03:ND_D1378")
-dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO3_CH6:,A=LS3_CD06:ND_D4647")
+dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO3_CH0:,A=FE_RFQ:ND_D1025:")
+dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO3_CH1:,A=LS1_CA01:ND_D1128:")
+dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO3_CH2:,A=LS1_CA03:ND_D1195:")
+dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO3_CH4:,A=LS1_CB01:ND_D1250:")
+dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO3_CH5:,A=LS1_CB03:ND_D1378:")
+dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO3_CH6:,A=LS3_CD06:ND_D4647:")
 
 # Slot 4:  Halo Monitor Rings (BLM)
-dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO4_CH0:,A=LS1_WA01:HMR_D1156")
-dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO4_CH1:,A=LS1_WA02:HMR_D1190")
-dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO4_CH2:,A=LS1_WA03:HMR_D1224")
-dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO4_CH4:,A=LS1_D:HMR_D1240")
-#dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO4_CH4:,A=LS1_WB01:HMR_D1287")
-dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO4_CH5:,A=LS1_WB02:HMR_D1351")
-dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO4_CH6:,A=LS1_WB03:HMR_D1415")
+dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO4_CH0:,A=LS1_WA01:HMR_D1156:")
+dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO4_CH1:,A=LS1_WA02:HMR_D1190:")
+dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO4_CH2:,A=LS1_WA03:HMR_D1224:")
+dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO4_CH4:,A=LS1_D:HMR_D1240:")
+#dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO4_CH4:,A=LS1_WB01:HMR_D1287:")
+dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO4_CH5:,A=LS1_WB02:HMR_D1351:")
+dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO4_CH6:,A=LS1_WB03:HMR_D1415:")
 
 ## Start the PICO python helper script
 system "python ../../iocBoot/scripts/blm_processing_thread.py DIAG_MTCA04:PICO3 DIAG_MTCA04:PICO4 &"
