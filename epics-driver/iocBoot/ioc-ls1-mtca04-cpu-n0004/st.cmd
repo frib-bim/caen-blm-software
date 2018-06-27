bf2327f0cf080666a5186a6f8650883ff17c4ed7
Bruno Martins <martins@frib.msu.edu>
2018-06-27 10:04:41 -0400

    MTCA04: fix D1224 D-Number, rename HMR being used for D-Station

--- epics-driver/iocBoot/ioc-ls1-mtca04-cpu-n0004/st.cmd
+++ epics-driver/iocBoot/ioc-ls1-mtca04-cpu-n0004/st.cmd
@@ -27,7 +27,7 @@ dbLoadRecords("../../db/pico8_frib.db","SYS=DIAG_MTCA04,D=PICO4,NAME=PICO4,NELM=
 # (SYS):(D)_CHX:Y_Z -> (A)Y_Z
 
 # Slot 3:  Neutron Detectors (BLM)
-dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO3_CH0:,A=FE_RFQ:ND_D1025")  
+dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO3_CH0:,A=FE_RFQ:ND_D1025")
 dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO3_CH1:,A=LS1_CA01:ND_D1128")
 dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO3_CH2:,A=LS1_CA03:ND_D1195")
 dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO3_CH4:,A=LS1_CB01:ND_D1250")
@@ -37,8 +37,9 @@ dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO3_CH6:,A=LS3_CD
 # Slot 4:  Halo Monitor Rings (BLM)
 dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO4_CH0:,A=LS1_WA01:HMR_D1156")
 dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO4_CH1:,A=LS1_WA02:HMR_D1190")
-dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO4_CH2:,A=LS1_WA03:HMR_D1223")
-dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO4_CH4:,A=LS1_WB01:HMR_D1287")
+dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO4_CH2:,A=LS1_WA03:HMR_D1224")
+dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO4_CH4:,A=LS1_D:HMR_D1240")
+#dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO4_CH4:,A=LS1_WB01:HMR_D1287")
 dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO4_CH5:,A=LS1_WB02:HMR_D1351")
 dbLoadRecords("../../db/pico8_chan_alias.db", "P=DIAG_MTCA04:PICO4_CH6:,A=LS1_WB03:HMR_D1415")
 
