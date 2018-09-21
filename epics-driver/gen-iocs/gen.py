6b375912f4089214ad068061e2b895ad585a3ac7
Bruno Martins <martins@frib.msu.edu>
2018-09-21 15:38:18 -0400

    Add generator for startup scrips

---
+++ epics-driver/gen-iocs/gen.py
@@ -0,0 +1,101 @@
+import xlrd
+import os
+from collections import defaultdict
+from jinja2 import Environment, FileSystemLoader
+
+source = 'Diagnostics Rack Wiring.xlsx'
+
+class MTCA:
+    def __init__(self, rack, mtca):
+        self.rack = rack
+        self.mtca = mtca
+        self.cards = {}
+
+    def __repr__(self):
+        return 'MTCA(%s rack=%s, cards: %r)' % (self.mtca, self.rack,
+                                                self.cards.__repr__())
+
+class Pico:
+    def __init__(self, slot):
+        self.slot = slot
+        self.chans = {}
+        self.addr = {
+            2: '0e', 3: '08', 4: '06', 5: '09',
+            6: '0b', 7: '0f', 8: '0d', 9: '07',
+            10: '05' }[slot]
+
+    def __repr__(self):
+        return self.chans.__repr__()
+
+class PicoChannel:
+    def __init__(self, chan, dev):
+        self.chan = chan
+        self.dev = dev
+
+    def __repr__(self):
+        return 'PicoCh(%s)' % (self.dev,)
+
+wb = xlrd.open_workbook(source, on_demand=True)
+racks = [sheet for sheet in wb.sheet_names() if 'CAB_N' in sheet]
+mtca = {}
+
+def get_mtca_map():
+    mtca = {}
+
+    for rack in racks:
+        ws = wb.sheet_by_name(rack)
+        header = ws.row_values(2)
+
+        cols = ('Lattice Beamline Location',
+                'Device Type', 'Function',
+                'MTCA Chassis', 'MTCA Slot', 'PICO Port')
+
+        rack = '_'.join(rack.split('_R1624CAB_'))
+        # All columns must be present in this sheet
+        if not set(cols).issubset(set(header)):
+            continue
+
+        # Read data columns
+        values = zip(*[ws.col_values(header.index(c), 3) for c in cols])
+
+        # Filter out incomplete data
+        values = [v for v in values if all(v)]
+
+        # Put data in dictionary
+        for loc, dev_type, dev_func, chassis, slot, port in values:
+            chassis = '%02d' % int(chassis)
+            slot = int(slot)
+            ch = int(port[2])
+
+            m = mtca.get(chassis)
+            if m is None:
+                m = mtca[chassis] = MTCA(rack, chassis)
+
+            c = m.cards.get(slot)
+            if c is None:
+                c = m.cards[slot] = Pico(slot)
+
+            if ch in c.chans:
+                print "Duplicate channel", c.chans[ch]
+
+            if dev_type in ('PM', 'EMS'):
+                dev = '%s:%s_' % (loc, dev_func.split(' Signal')[0])
+            else:
+                dev = '%s:' % loc
+
+            c.chans[ch] = PicoChannel(ch, dev)
+
+    return mtca
+
+mtca_map = get_mtca_map()
+
+env = Environment(loader=FileSystemLoader('./'))
+template = env.get_template('template.st.cmd')
+
+for mtca_num, mtca_data in mtca_map.items():
+    target = '../iocBoot/ioc-mtca%s-pico8/st.cmd' % mtca_num
+    with open(target, 'w') as f:
+        f.write(template.render({'mtca_num': mtca_num,
+                                 'mtca_data': mtca_data}))
+    print target
+
