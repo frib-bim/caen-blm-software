8dc6437c2dd94e878cf658a351db14b594b6d1a5
Cogan, Scott <cogan@frib.msu.edu>
2019-07-11 10:15:46 -0400

    SDC: Update python script for proper scale factor.

--- epics-driver/iocBoot/scripts/blm_processing_thread.py
+++ epics-driver/iocBoot/scripts/blm_processing_thread.py
@@ -7,6 +7,7 @@
 """
 Initiate from console:  python blm_processing_thread.py <picoDevPrefix1> <picoDevPrefix2> ...
 @author: cogan
+2019-07-11	cogan	Syntax update python2->3.  Added SLO_CSET scale factors.
 """
 
 # Import and define common utilities, including matlab plots, math, sleep, and EPICS CA
@@ -27,14 +28,15 @@ def acqCBufCallback(value):
     numpts = dev.get("_CTRL:acqDDRnpts");  # numpts = pow(2,18);
     dat = getCBufData(dev, numpts);  # Get/process raw waveform
     #  Assign the soft channel outputs
-    dev.set('_CH0:DDRWV_RD', dat.ch0 * 1e6);
-    dev.set('_CH1:DDRWV_RD', dat.ch1 * 1e6);
-    dev.set('_CH2:DDRWV_RD', dat.ch2 * 1e6);
-    dev.set('_CH3:DDRWV_RD', dat.ch3 * 1e6);
-    dev.set('_CH4:DDRWV_RD', dat.ch4 * 1e6);
-    dev.set('_CH5:DDRWV_RD', dat.ch5 * 1e6);
-    dev.set('_CH6:DDRWV_RD', dat.ch6 * 1e6);
-    dev.set('_CH7:DDRWV_RD', dat.ch7 * 1e6);
+    #  Scale by SLO (units A -> uA)
+    dev.set('_CH0:DDRWV_RD', dat.ch0 * dev.get('_CH0:SLO_CSET'));
+    dev.set('_CH1:DDRWV_RD', dat.ch1 * dev.get('_CH1:SLO_CSET'));
+    dev.set('_CH2:DDRWV_RD', dat.ch2 * dev.get('_CH2:SLO_CSET'));
+    dev.set('_CH3:DDRWV_RD', dat.ch3 * dev.get('_CH3:SLO_CSET'));
+    dev.set('_CH4:DDRWV_RD', dat.ch4 * dev.get('_CH4:SLO_CSET'));
+    dev.set('_CH5:DDRWV_RD', dat.ch5 * dev.get('_CH5:SLO_CSET'));
+    dev.set('_CH6:DDRWV_RD', dat.ch6 * dev.get('_CH6:SLO_CSET'));
+    dev.set('_CH7:DDRWV_RD', dat.ch7 * dev.get('_CH7:SLO_CSET'));
     dev.set('_CTRL:DDR_TIME_RD', dat.t0);
     dev.set('_CTRL:DDR_BEAMSTATW_RD', dat.beamstat);
     dev.set('_CTRL:DDR_NOKW_RD', dat.nokflags);
