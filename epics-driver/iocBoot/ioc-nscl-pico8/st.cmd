ec87d3ca06adbd233cbaafd9387af55922c3c3b7
Bruno Martins <martins@frib.msu.edu>
2019-08-21 09:51:11 -0400

    Fix NSCL IOC

--- epics-driver/iocBoot/ioc-nscl-pico8/st.cmd
+++ epics-driver/iocBoot/ioc-nscl-pico8/st.cmd
@@ -10,7 +10,9 @@ dbLoadDatabase("../../dbd/pico.dbd",0,0)
 pico_registerRecordDeviceDriver(pdbbase)
 
 createPICO8("PICO", "/dev/amc_pico_0000:06:00.0")
-dbLoadRecords("../../db/pico8_frib.db","SYS=EGUN,D=PICO,NAME=PICO,NELM=1000000")
+dbLoadRecords("../../db/pico8_frib.db","SYS=DIAG,SSYS=EGUN,NAME=PICO")
+
+reAddAlias "DIAG_EGUN(.*)" "EGUN$1"
 
 system "python ../../iocBoot/scripts/blm_processing_thread.py EGUN:PICO &"
 
