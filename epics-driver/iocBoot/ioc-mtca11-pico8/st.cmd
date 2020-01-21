6cbaa33e805402dc988444a2e7c9d32bbe06892c
Cogan, Scott <cogan@frib.msu.edu>
2020-01-21 15:52:24 -0500

    SDC: Calibrated ASLO for FC_D2634

--- epics-driver/iocBoot/ioc-mtca11-pico8/st.cmd
+++ epics-driver/iocBoot/ioc-mtca11-pico8/st.cmd
@@ -18,8 +18,8 @@ createPICO8("PICO7", "/dev/amc_pico_0000:0f:00.0")
 createPICO8("PICO9", "/dev/amc_pico_0000:07:00.0")
 
 # (SYS):(D)_CHX:Y_Z
-# PICO7 CH4 (0-based) has 1/8 current divider
-dbLoadRecords("../../db/pico8_frib.db","SYS=DIAG,SSYS=MTCA11,NAME=PICO7,ASLO4=8e6")
+# PICO7 CH4 (0-based) has 1/8 current divider (calibrated value)
+dbLoadRecords("../../db/pico8_frib.db","SYS=DIAG,SSYS=MTCA11,NAME=PICO7,ASLO4=7.84e6")
 dbLoadRecords("../../db/pico8_frib.db","SYS=DIAG,SSYS=MTCA11,NAME=PICO9")
 
 # record name aliases
